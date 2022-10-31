d95c125cbbe04aa85410b77b0f13e7a36e77a1f1
NovikovaNadezda <114093562+NovikovaNadezda@users.noreply.github.com>
2022-10-31 18:40:39 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	/*const int size = 5;
+	int mas[size];
+	int mas1[] = { 1,2,3,4,5 };
+	for (int i = 0; i < size; i++) {
+		cin >> mas[i];
+	}
+	for (int i = 0; i < size; i++) {
+		cout << mas[i] << ' ';
+	}
+	cout << endl;*/
+	//дан массив из 10 элементов. Найти кол-во четных и количество нечетныхв этом массиве
+	/*const int size = 10;
+	int arr[size];
+	for (int i = 0; i < size; i++) {
+		cout << arr[i];
+	}
+	int even_qty = 0, odd_qty = 0;
+	for (int i = 0; i < size; i++) {
+		cout << (arr[i] % 2 == 0) {
+			even_qty++;
+		}
+		else {*/
+		//дан массив из 8 элементов. Найти сумму положительных чисел в этом массиве
+	/*int mas[size];
+	int mas1[] = { 1,2,3,4,5 };
+	for (int i = 0; i < size; i++) {
+		cin >> mas[i];
+	}
+	for (int i = 0; i < size; i++) {
+		cout << mas[i] << ' ';*/
+	//Array20.Дан массив размера N и целые числа K и L(1 ≤ K ≤ L ≤ N).Найти
+	//сумму элементов массива с номерами от K до L включительно.
+}
